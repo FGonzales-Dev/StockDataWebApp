65abee0352276ead2d6b2767470f9521d87f6c30
francis gonzales <fgonzales.private@icloud.com>
2022-09-29 18:31:16 +0800

    Update scraperVersionTwo.py

--- core/scraperVersionTwo.py
+++ core/scraperVersionTwo.py
@@ -141,7 +141,7 @@ def scrape(request):
             with open("dividends.xls", 'rb') as file:
                 response = HttpResponse(file, content_type='application/vnd.ms-excel')
                 response['Content-Disposition'] = 'attachment; filename=stockData.xls'   
-            return HttpResponse("hello", content_type='text/json')
+            return render(request,res, content_type='text/json')
         elif download_type == "OPERATING_PERFORMANCE":
                 res = AsyncResult(task_id).get()
                 df = pd.read_json(res)
